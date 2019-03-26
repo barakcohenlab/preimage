5581bbc0e1c0d0d6e2ece2ca22bcb119a78806ad
rfriedman22 <ryanfriedman22@gmail.com>
2019-03-26 14:56:39 -0500

    Numpy aesthetics in setup

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-import numpy
+import numpy as np
 from Cython.Distutils import build_ext
 
 
 ext_modules = [
     Extension("preimage.kernels._generic_string", ["preimage/kernels/_generic_string.pyx"],
-              include_dirs=[numpy.get_include()]),
+              include_dirs=[np.get_include()]),
     Extension("preimage.inference.branch_and_bound", ["preimage/inference/branch_and_bound.pyx"],
-              include_dirs=[numpy.get_include()]),
+              include_dirs=[np.get_include()]),
     Extension("preimage.inference.bound_calculator", ["preimage/inference/bound_calculator.pyx"],
-              include_dirs=[numpy.get_include()]),
+              include_dirs=[np.get_include()]),
     Extension("preimage.inference.node", ["preimage/inference/node.pyx"],
-              include_dirs=[numpy.get_include()]),
+              include_dirs=[np.get_include()]),
     Extension("preimage.inference.node_creator", ["preimage/inference/node_creator.pyx"],
-              include_dirs=[numpy.get_include()]),
+              include_dirs=[np.get_include()]),
     Extension("preimage.features.gs_similarity_weights", ["preimage/features/gs_similarity_weights.pyx"],
-              include_dirs=[numpy.get_include()])
+              include_dirs=[np.get_include()])
 ]
 
 setup(
