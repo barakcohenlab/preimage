deb8e6b209435bd70e9074d6119d78602402be68
rfriedman22 <ryanfriedman22@gmail.com>
2019-03-26 16:46:02 -0500

    Support to load in DNA shape similarity tables

--- preimage/datasets/loader.py
+++ preimage/datasets/loader.py
@@ -1,14 +1,15 @@
 """Loader for datasets and amino acids"""
 
-__author__ = 'amelie'
+__author__ = "amelie"
 
 import pickle
 import gzip
-from os.path import dirname, join
+import os
 
 import numpy as np
 
 from preimage.datasets.amino_acid_file import AminoAcidFile
+from preimage.datasets.dna_shape_files import DnaShapeFiles
 
 
 class StructuredOutputDataset:
@@ -62,19 +63,19 @@ def load_ocr_letters(fold_id=0):
     test_dataset : StructuredOutputDataset
         Testing dataset.
     """
-    data = __load_gz_pickle_file('ocrletters.pickle.gz')
-    train_indexes = np.where(data['fold_ids'] == fold_id)
-    test_indexes = np.where(data['fold_ids'] != fold_id)
-    Y_train = np.array(data['y'], dtype=np.str)[train_indexes]
-    Y_test = np.array(data['y'], dtype=np.str)[test_indexes]
-    train_dataset = StructuredOutputDataset(data['X'][train_indexes], Y_train)
-    test_dataset = StructuredOutputDataset(data['X'][test_indexes], Y_test)
+    data = __load_gz_pickle_file("ocrletters.pickle.gz")
+    train_indexes = np.where(data["fold_ids"] == fold_id)
+    test_indexes = np.where(data["fold_ids"] != fold_id)
+    Y_train = np.array(data["y"], dtype=np.str)[train_indexes]
+    Y_test = np.array(data["y"], dtype=np.str)[test_indexes]
+    train_dataset = StructuredOutputDataset(data["X"][train_indexes], Y_train)
+    test_dataset = StructuredOutputDataset(data["X"][test_indexes], Y_test)
     return train_dataset, test_dataset
 
 
 def __load_gz_pickle_file(file_name):
-    module_path = dirname(__file__)
-    gzip_reader = gzip.open(join(module_path, file_name), 'rb')
+    module_path = os.path.dirname(__file__)
+    gzip_reader = gzip.open(os.path.join(module_path, file_name), "rb")
     data = pickle.loads(gzip_reader.read())
     return data
 
@@ -87,7 +88,7 @@ def load_camps_dataset():
     train_dataset: StandardDataset
         Training dataset.
     """
-    return __load_peptide_dataset('camps.pickle')
+    return __load_peptide_dataset("camps.pickle")
 
 
 def load_bpps_dataset():
@@ -98,19 +99,19 @@ def load_bpps_dataset():
     train_dataset: StandardDataset
         Training dataset.
     """
-    return __load_peptide_dataset('bpps.pickle')
+    return __load_peptide_dataset("bpps.pickle")
 
 
 def __load_peptide_dataset(file_name):
     data = __load_pickle_file(file_name)
-    X = np.array(data['X'], dtype=np.str)
-    train_dataset = StandardDataset(X, data['y'])
+    X = np.array(data["X"], dtype=np.str)
+    train_dataset = StandardDataset(X, data["y"])
     return train_dataset
 
 
 def __load_pickle_file(file_name):
-    module_path = dirname(__file__)
-    data_file = open(join(module_path, file_name), 'rb')
+    module_path = os.path.dirname(__file__)
+    data_file = open(os.path.join(module_path, file_name), "rb")
     data = pickle.load(data_file)
     return data
 
@@ -131,10 +132,39 @@ def load_amino_acids_and_descriptors(file_name=AminoAcidFile.blosum62_natural):
         Substitution cost of each amino acid with all the other amino acids, where n_amino_acids is the number of
         amino acids.
     """
-    path_to_file = join(dirname(__file__), 'amino_acid_matrix', file_name)
-    with open(path_to_file, 'r') as data_file:
+    path_to_file = os.path.join(os.path.dirname(__file__), "amino_acid_matrix", file_name)
+    with open(path_to_file, "r") as data_file:
         lines = data_file.readlines()
     splitted_lines = np.array([line.split() for line in lines])
     amino_acids = [str(letter) for letter in splitted_lines[:, 0]]
     descriptors = np.array(splitted_lines[:, 1:], dtype=np.float)
     return amino_acids, descriptors
+
+
+def load_dna_pentamers_and_shape_similarity(file_names=DnaShapeFiles.dna_shape_core):
+    """Load DNA pentamers and the 6 DNA shape lookup tables.
+
+    Parameters
+    ----------
+    file_names : dict, {str: str}
+        File names for the 6 DNA shape similarity matrices. Keys are types of comparisons, values are the filenames.
+
+    Returns
+    -------
+    pentamers: list
+        A list of DNA pentamers (strings). Reverse compliments are excluded, e.g. if AAAAA is present then TTTTT is not.
+    similarity_tables: dict, {str: array, shape = (n_pentamers, n_pentamers)}
+        DNA shape similarity table of each pentamer with all the other pentamers, where n_pentamers is the number of
+        DNA pentamers. There is one table for each type of comparison (e.g. L-to-L, L-to-M). Keys are the comparison
+        type, values are the similarity tables.
+    """
+    path_to_files = os.path.join(os.path.dirname(__file__), "dna_shape_matrix")
+    similarity_tables = {}
+    for comparison, file in file_names.items():
+        with open(os.path.join(path_to_files, file), "r") as data_file:
+            lines = data_file.readlines()
+        splitted_lines = np.array([line.split() for line in lines])
+        pentamers = [str(pentamer) for pentamer in splitted_lines[:, 0]]
+        similarity_tables[comparison] = np.array(splitted_lines[:, 1:], dtype=np.float)
+
+    return pentamers, similarity_tables
