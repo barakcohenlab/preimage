d7507e13a01750c4cd8918544fb07af0ccc7bfd1
rfriedman22 <ryanfriedman22@gmail.com>
2019-07-24 11:24:56 -0500

    Refactored DNA shape loaders for one file

--- preimage/datasets/dna_shape_files.py
+++ preimage/datasets/dna_shape_files.py
@@ -1,38 +1,15 @@
 __author__ = "Ryan Z. Friedman"
 
 
-def generate_all_six_files(mat_type):
-    """Generate the file name for all 6 DNA shape similarity matrices.
-
-    Parameters
-    ----------
-    mat_type : string
-        The type of shape similarity matrices to load, should be the end of the filename before .txt.
-
-    Returns
-    -------
-    similarity_files : dict, {str: str}
-        Keys are types of shape comparisons, values are the filenames.
-
-    """
-    prefix = "dnaShapeSimilarity"
-    similarity_keys = ["LeftToLeft", "LeftToMid", "LeftToRight", "MidToMid", "MidToRight", "RightToRight"]
-    similarity_files = {i: f"{prefix}{i}{mat_type}.txt" for i in similarity_keys}
-    return similarity_files
-
-
 class DnaShapeFiles:
-    """File names of DNA shape similarity matrices. For each set of DNA shape features, there are six similarity
-    matrices to handle all the edge cases.
+    """File names of DNA shape similarity matrices.
 
     Attributes
     ----------
-    dna_shape_core : dict, {str: str}
-        File names for the 6 similarity matrices for the 4 core DNA shape parameters: MGW, ProT, HelT, Roll.
-        Keys are types of shape comparisons, values are the filenames.
-    dna_shape_full : dict, {str: str}
-        File names for the 6 similarity matrices for the full set of 14 DNA shape parameters.
-        Keys are types of shape comparisons, values are the filenames.
+    dna_shape_core : str
+        File names for the similarity matrix for the 4 core DNA shape parameters: MGW, ProT, HelT, Roll.
+    dna_shape_full : str
+        File names for the similarity matrix for the full set of 14 DNA shape parameters.
     """
-    dna_shape_core = generate_all_six_files("Core")
-    dna_shape_full = generate_all_six_files("Full")
+    dna_shape_core = "dna_shape_similarity_core.txt"
+    dna_shape_full = "dna_shape_similarity_full.txt"
--- preimage/datasets/loader.py
+++ preimage/datasets/loader.py
@@ -141,13 +141,13 @@ def load_amino_acids_and_descriptors(file_name=AminoAcidFile.blosum62_natural):
     return amino_acids, descriptors
 
 
-def load_dna_pentamers_and_shape_similarity(file_names=DnaShapeFiles.dna_shape_core, sigma_physical=1.0):
-    """Load DNA pentamers and the 6 DNA shape lookup tables, then normalize and exponentiate the matrix.
+def load_dna_pentamers_and_shape_similarity(file_name=DnaShapeFiles.dna_shape_core, sigma_physical=1.0):
+    """Load DNA pentamers and the DNA shape lookup tables, then normalize and exponentiate the matrix.
 
     Parameters
     ----------
-    file_names : dict, {str: str}
-        File names for the 6 DNA shape similarity matrices. Keys are types of comparisons, values are the filenames.
+    file_names : str
+        File name for the DNA shape similarity matrix.
     sigma_physical : float
         Normalization hyperparameter for physicochemical properties. After loading the distance matrix, normalize
 
@@ -155,29 +155,20 @@ def load_dna_pentamers_and_shape_similarity(file_names=DnaShapeFiles.dna_shape_c
     -------
     pentamers: list
         A list of DNA pentamers (strings). Reverse compliments are excluded, e.g. if AAAAA is present then TTTTT is not.
-    similarity_tables: dict, {str: array, shape = (n_pentamers, n_pentamers)}
+    similarity_table: array, shape = [n_pentamers, n_pentamers]
         DNA shape similarity table of each pentamer with all the other pentamers, where n_pentamers is the number of
-        DNA pentamers, normalized to the sigma hyperparameter, and then exponentiated. There is one table for each
-        type of comparison (e.g. L-to-L, L-to-M). Keys are the comparison type, values are the similarity tables.
+        DNA pentamers, normalized to the sigma hyperparameter, and then exponentiated.
     """
-    #FIXME need a better implementation of this for when sigma is zero. Perhaps something that will make all of this
-    # easier is if there is only one DNA shape similarty table and in order to deal with edge cases, you simply tack
-    # on an extra 2 bp from the sequence context.
-    path_to_files = os.path.join(os.path.dirname(__file__), "dna_shape_matrix")
-    similarity_tables = {}
-    for comparison, file in file_names.items():
-        with open(os.path.join(path_to_files, file), "r") as data_file:
-            lines = data_file.readlines()
-        splitted_lines = np.array([line.split() for line in lines])
-        pentamers = [str(pentamer) for pentamer in splitted_lines[:, 0]]
-        # If sigma_physical is zero, then the similarity matrix reduces to the identity matrix.
-        if sigma_physical == 0:
-            similarity = np.identity(len(pentamers))
-        else:
-            similarity = np.array(splitted_lines[:, 1:], dtype=np.float)
-            similarity /= (2.0 * (sigma_physical ** 2))
-            similarity = np.exp(-similarity)
-
-        similarity_tables[comparison] = similarity
-
-    return pentamers, similarity_tables
+    path_to_file = os.path.join(os.path.dirname(__file__), "dna_shape_matrix", file_name)
+    with open(path_to_file, "r") as data_file:
+        lines = data_file.readlines()
+    splitted_lines = np.array([line.split() for line in lines])
+    pentamers = [str(pentamer) for pentamer in splitted_lines[:, 0]]
+    # If sigma_physical is zero, then the similarity amtrix reduces to the identity matrix
+    if sigma_physical == 0:
+        similarity_table = np.identity(len(pentamers))
+    else:
+        similarity_table = np.array(splitted_lines[:, 1:], dtype=np.float)
+        similarity_table /= (2.0 * (sigma_physical**2))
+        similarity_table = np.exp(-similarity_table)
+    return pentamers, similarity_table
